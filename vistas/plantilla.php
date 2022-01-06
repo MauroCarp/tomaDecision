00ac7b6268f5178059c56618a23e1e6157a6e04a
Mauro Gonzalez <maurogt.dd@gmail.com>
2022-01-05 22:33:00 -0300

    Fix: Remember me, cambio de funcion, se agregaron las variables SESSION

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -171,22 +171,38 @@ CUERPO DOCUMENTO
  
   <?php
 
-function encrypt_decrypt($action, $string) {
-  $output = false;
+function encrypt_decrypt($action, $string){
 
-  $encrypt_method = "AES-128-ECB";
-  $key = 'remember';
+    $output = false;
 
-  if ( $action == 'encrypt' ) {
-      $output = openssl_encrypt($string, $encrypt_method, $key);
-      $output;
-  } else if( $action == 'decrypt' ) {
-      $output = openssl_decrypt($string, $encrypt_method, $key);
-  }
+    $encrypt_method = "AES-256-CBC";
 
-  return $output;
+    $secret_key = 'WS-SERVICE-KEY';
+
+    $secret_iv = 'WS-SERVICE-VALUE';
+    // hash
+    $key = hash('sha256', $secret_key);
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+    if ($action == 'encrypt') {
+
+        $output = base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
+
+    } else {
+
+        if ($action == 'decrypt') {
+
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+
+        }
+
+    }
+
+    return $output;
+
 }
-
 
   if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"){
 
@@ -194,17 +210,23 @@ function encrypt_decrypt($action, $string) {
 
 
   }else if( isset($_COOKIE['rememberme']  )){
-   
-    // Decrypt cookie variable value
-    $userid = encrypt_decrypt('decrypt',$_COOKIE['rememberme']);
-               
-    $item = 'id';
-
-    $count = ControladorUsuarios::ctrContarUsuarios($item,$userid);
     
+    $userid = encrypt_decrypt('decrypt',$_COOKIE['rememberme']);
+            
+    $item = 'id';
+    
+    $count = ControladorUsuarios::ctrContarUsuarios($item,$userid);
+
     if( $count > 0 ){
 
+      $user = ControladorUsuarios::ctrMostrarUsuarios($item,$userid);
+
         $_SESSION['id'] = $userid; 
+        $_SESSION["iniciarSesion"] = "ok";
+        $_SESSION["nombre"] = $user["nombre"];
+        $_SESSION["usuario"] = $user["usuario"];
+        $_SESSION["foto"] = $user["foto"];
+        $_SESSION["perfil"] = $user["perfil"];
        
         include "cuerpo.php";
 
