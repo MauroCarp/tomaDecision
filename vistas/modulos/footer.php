6dd71693fcd8d1fc85d65bcaa32098b7f973aa6d
Mauro <maurogt.dd@gmail.com>
2024-03-10 17:27:14 -0300

    v2

--- vistas/modulos/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-	
-	<strong>Copyright &copy; 2022 Toma de Decision</strong>
-
-	Todos los derechos reservados
-
-</footer>
--- vistas/modulos/inicio/grafico.php
+++
@@ -1,3 +0,0 @@
-<div style="margin:0 auto;width:60%">
-	<canvas id="chart-area"></canvas>
-</div>
